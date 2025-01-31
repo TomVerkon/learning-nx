f0ef9b6c8f9706a9825015d21a16836ed0bac6df
TomVerkon <tverkon@gmail.com>
2025-01-31 16:05:00 -0500

    Broke when using nx to gen

--- libs/schema.d.ts
+++ libs/schema.d.ts
@@ -1,3 +1,5 @@
 export interface GenerateAngularLibraryGeneratorSchema {
   name: string;
+  domain: 'finance' | 'hr' | 'marketing' | 'inventory' | 'shared';
+  type: 'ui' | 'data-access' | 'feature' | 'util' | 'all';
 }
